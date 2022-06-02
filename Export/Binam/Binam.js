a61a661451038e25cc0b97e181502ca5f03d6e10
Zepe237 <vadel.tsague@gmail.com>
2022-06-02 18:51:05 +0200

    creation d'un formulaire avec son serveur sans sql

---
+++ Export/Binam/Binam.js
@@ -0,0 +1,1 @@
+var express = require('express'); var app = express(); const bp = require('body-parser'); app.use(bp.json()); app.use(bp.urlencoded({ extended: true }));app.post('/Binam', function(request, response){console.log(request.body);response.send('bonjour');});app.listen(8081); console.log('Server is listening on port 8081');
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 var nameF ;
 // premiere partie du code html de chaque fichier html creer
-var debut = '<!DOCTYPE html><html lang="en"><head> <link rel="stylesheet" href="style.css"><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><div class="contain"><form method="post">';
+var debut = '<!DOCTYPE html><html lang="en"><head> <link rel="stylesheet" href="style.css"><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><div class="contain">';
 // titre de notre formulaire
 var titre1 ='<h3>';
 var titre2 ='</h3> </br>';
@@ -13,10 +13,62 @@ var titre2 ='</h3> </br>';
 var fin = '</form></div></body></html>' ;
 // Code a inserer dans le fichier css
 var codeCss = '/*Code css */.contain{padding: 50px;border: 4px aqua solid;text-align: center;float: center;border-radius: 10px;margin-left: 400px;margin-right: 400px;margin-top: 10px;}input{border-radius: 5px;margin: 5px;padding: 2px;}body{text-align: center;margin: 0;padding: 0;}';
+// code a inserer dans le fichier JS
+var debutJs = "var express = require('express'); var app = express(); const bp = require('body-parser'); app.use(bp.json()); app.use(bp.urlencoded({ extended: true }));" ;
+var finJS = "app.listen(8081); console.log('Server is listening on port 8081');"
 // simple route
 app.post('/', function(request, response){
   console.log(request.body.form);
 });
+// appel a graphDB
+const { EnapsoGraphDBClient } = require("@innotrade/enapso-graphdb-client");
+
+// Config
+const GRAPHDB_BASE_URL = "http://localhost:7200",
+    GRAPHDB_REPOSITORY = "AppBuilder",
+    GRAPHDB_USERNAME = "admin",
+    GRAPHDB_PASSWORD = "admin",
+    GRAPHDB_CONTEXT_TEST = "http://www.semanticweb.org/vadeltsague/ontologies/2022/4/untitled-ontology-6";
+const DEFAULT_PREFIXES = [
+    EnapsoGraphDBClient.PREFIX_OWL,
+    EnapsoGraphDBClient.PREFIX_RDF,
+    EnapsoGraphDBClient.PREFIX_RDFS,
+    EnapsoGraphDBClient.PREFIX_XSD,
+    EnapsoGraphDBClient.PREFIX_PROTONS,
+    {
+        prefix: "App",
+        iri: "http://www.semanticweb.org/vadeltsague/ontologies/2022/4/untitled-ontology-6#form",
+    }
+];
+
+//Create an Endpoint.
+let graphDBEndpoint = new EnapsoGraphDBClient.Endpoint({
+  baseURL: GRAPHDB_BASE_URL,
+  repository: GRAPHDB_REPOSITORY,
+  prefixes: DEFAULT_PREFIXES
+});
+
+//Authenticate (Optional)
+graphDBEndpoint.login(GRAPHDB_USERNAME,GRAPHDB_PASSWORD)
+.then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.log(err);
+});
+// insert
+graphDBEndpoint
+  .update(
+    `insert data {
+      graph <${GRAPHDB_CONTEXT_TEST}> {
+      App:Form App:est_constitué_de App:Input }
+  }`
+         )
+  .then((result) => {
+    console.log("inserted a class :\n" + JSON.stringify(result, null, 2));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
 // set the view engine to ejs
@@ -54,22 +106,26 @@ app.post('/build', function(request, res){
 // arriver a la page download en provenant de build
 app.post('/download', function(req, res){
   console.log('Je suis appele')
+  // ajout de l'action a mon formulaire creer 
+  action = '<form method="POST" action= "http://localhost:8081/'+nameF + '">'
   // recuperation du code html du formulaire dans le post
   var formulaire = req.body.formulaire ;
-  // creation d'un sous dossier dans le dossier Export 
+  // creation d'un sous dossier dans le dossier Export
+  //creation de la route post dans æe fichier Js generer
+  var corpsJs = "app.post('/" +nameF +"', function(request, response){console.log(request.body);response.send('bonjour');});"; 
   fs.mkdir(path.join(__dirname+'/Export/', nameF), (err) => {
       if (err) {
           return console.error(err);
       }
       console.log('Directory created successfully!');
   });
-  // creation du fichier html dans le dossier creer
-  fs.writeFile(__dirname+'/Export/'+ nameF+ '/'+nameF+'.html', debut +titre1+nameF+titre2+ formulaire + fin, function (err) {
+  // creation du fichier html dans le dossier creertitre1 +
+  fs.writeFile(__dirname+'/Export/'+ nameF+ '/'+nameF+'.html', debut +action +titre1+nameF+titre2+ formulaire + fin, function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
     // creation du fichier Js dans le dossier creer
-  fs.writeFile(__dirname+'/Export/'+ nameF+ '/'+nameF+'.js', 'Code Js', function (err) {
+  fs.writeFile(__dirname+'/Export/'+ nameF+ '/'+nameF+'.js', debutJs + corpsJs +finJS, function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
